15cdc3144236654f47899810f0b864aef9550777
Kevin Estrella <kevsestrella@gmail.com>
2020-06-28 14:43:19 +0800

    Initial commit

---
+++ download.sh
@@ -0,0 +1,4 @@
+ /opt/intel/openvino/deployment_tools/open_model_zoo/tools/downloader/downloader.py --name face-detection-adas-binary-0001
+ /opt/intel/openvino/deployment_tools/open_model_zoo/tools/downloader/downloader.py --name head-pose-estimation-adas-0001
+ /opt/intel/openvino/deployment_tools/open_model_zoo/tools/downloader/downloader.py --name landmarks-regression-retail-0009
+ /opt/intel/openvino/deployment_tools/open_model_zoo/tools/downloader/downloader.py --name gaze-estimation-adas-0002
