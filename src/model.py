fef72bcc5199d46dc97f3697ca799fd4a363f66f
Kevin Estrella <kevsestrella@gmail.com>
2020-06-30 18:51:17 +0800

    Implemented Model_X as Face Detection ABC

--- src/model.py
+++ src/model.py
@@ -1,46 +1,68 @@
-'''
-This is a sample class for a model. You may choose to use it as-is or make any changes to it.
-This has been provided just to give you an idea of how to structure your model class.
-'''
+from abc import ABC, abstractmethod
 
-class Model_X:
+import cv2
+from openvino.inference_engine import IENetwork, IECore
+
+
+class Model_X(ABC):
     '''
-    Class for the Face Detection Model.
+    Abstract Class for the Face Detection Model.
     '''
-    def __init__(self, model_name, device='CPU', extensions=None):
-        '''
-        TODO: Use this to set your instance variables.
-        '''
-        raise NotImplementedError
+    def __init__(self, model_name, device='CPU', threshold=0.5, extensions=None):
+        self.model_weights = model_name + '.bin'
+        self.model_structure = model_name + '.xml'
+        self.device = device
+        self.threshold = threshold
+        self.extensions = extensions
 
     def load_model(self):
         '''
-        TODO: You will need to complete this method.
         This method is for loading the model to the device specified by the user.
         If your model requires any Plugins, this is where you can load them.
         '''
-        raise NotImplementedError
+        self.network = IENetwork(self.model_structure, self.model_weights)
+        self.input_name = next(iter(self.network.inputs))
+        self.input_shape = self.network.inputs[self.input_name].shape
+        self.output_name = next(iter(self.network.outputs))
+        self.output_shape = self.network.outputs[self.output_name].shape
 
+        self.core = IECore()
+
+        if self.extensions and "CPU" in self.device:
+            self.core.add_extension(self.extensions, self.device)
+
+        supported_layers = self.core.query_network(network=self.network, device_name=self.device)
+        unsupported_layers = [l for l in self.network.layers.keys() if l not in supported_layers]
+
+        if unsupported_layers:
+            log.error(f"Unsupported layers found: {unsupported_layers}")
+            exit(1)
+
+        self.exec_network = self.core.load_network(self.network, self.device)
+
+    @abstractmethod
     def predict(self, image):
         '''
-        TODO: You will need to complete this method.
-        This method is meant for running predictions on the input image.
+        This abstractmethod is meant for running predictions on the input image.
         '''
-        raise NotImplementedError
+        pass
 
     def check_model(self):
         raise NotImplementedError
 
     def preprocess_input(self, image):
-    '''
-    Before feeding the data into the model for inference,
-    you might have to preprocess it. This function is where you can do that.
-    '''
-        raise NotImplementedError
+        '''
+        Before feeding the data into the model for inference,
+        you might have to preprocess it. This function is where you can do that.
+        '''
+        ret = cv2.resize(image, (self.input_shape[3], self.input_shape[2]))
+        ret = ret.transpose((2, 0, 1))
+        ret = ret.reshape(1, *ret.shape)
 
+    @abstractmethod
     def preprocess_output(self, outputs):
-    '''
-    Before feeding the output of this model to the next model,
-    you might have to preprocess the output. This function is where you can do that.
-    '''
-        raise NotImplementedError
+        '''
+        Before feeding the output of this model to the next model,
+        you might have to preprocess the output. This function is where you can do that.
+        '''
+        pass
