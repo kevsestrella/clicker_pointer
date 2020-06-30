b0b1c4b2963c77a9d82efe2ed056418eacb2cba3
Kevin Estrella <kevsestrella@gmail.com>
2020-06-30 18:53:09 +0800

    Used Model_X ABC to FaceDetector implementation

--- src/face_detection.py
+++ src/face_detection.py
@@ -1,3 +1,4 @@
+from model import Model_X
 import logging as log
 
 import cv2
@@ -5,45 +6,13 @@ import cv2
 from openvino.inference_engine import IENetwork, IECore
 
 
-class FaceDetector:
+class FaceDetector(Model_X):
     '''
     Class for the Face Detection Model.
     '''
     def __init__(self, model_name, device='CPU', threshold=0.5, extensions=None):
-        self.model_weights = model_name + '.bin'
-        self.model_structure = model_name + '.xml'
-        self.device = device
-        self.threshold = threshold
-        self.extensions = extensions
-        self.face_image = None
-        self.face_coordinates = None
-        self.exec_network = None
+        super().__init__(model_name, device=device, threshold=threshold, extensions=extensions)
 
-
-    def load_model(self):
-        '''
-        This method is for loading the model to the device specified by the user.
-        If your model requires any Plugins, this is where you can load them.
-        '''
-        self.model = IENetwork(self.model_structure, self.model_weights)
-        self.input_name = next(iter(self.model.inputs))
-        self.input_shape = self.model.inputs[self.input_name].shape
-        self.output_name = next(iter(self.model.outputs))
-        self.output_shape = self.model.outputs[self.output_name].shape
-
-        self.plugin = IECore()
-
-        if self.extensions and "CPU" in self.device:
-            self.plugin.add_extension(self.extensions, self.device)
-
-        supported_layers = self.plugin.query_network(network=self.model, device_name=self.device)
-        unsupported_layers = [l for l in self.model.layers.keys() if l not in supported_layers]
-
-        if unsupported_layers:
-            log.error(f"Unsupported layers found: {unsupported_layers}")
-            exit(1)
-
-        self.exec_network = self.plugin.load_network(self.model, self.device)
 
     def predict(self, image):
         '''
@@ -55,20 +24,6 @@ class FaceDetector:
         face_image = image[coordinates[1]:coordinates[3], coordinates[0]:coordinates[2]]
         return coordinates, face_image
 
-
-    def check_model(self):
-        raise NotImplementedError
-
-    def preprocess_input(self, image):
-        '''
-        Before feeding the data into the model for inference,
-        you might have to preprocess it. This function is where you can do that.
-        '''
-        ret = cv2.resize(image, (self.input_shape[3], self.input_shape[2]))
-        ret = ret.transpose((2, 0, 1))
-        ret = ret.reshape(1, *ret.shape)
-
-        return ret
 
     def preprocess_output(self, outputs, image_shape):
         '''
