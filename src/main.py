77dcb1d2415ab541bd056960501ff3da4a4516f9
Kevin Estrella <kevsestrella@gmail.com>
2020-07-04 16:22:51 +0800

    Linting

--- src/main.py
+++ src/main.py
@@ -29,17 +29,13 @@ def build_argparser():
             help="Path to image/video file or CAM")
     parser.add_argument("-d", "--device", type=str, default="CPU",
                     help="Specify the target device to infer on: "
-                         "CPU, GPU, FPGA or MYRIAD is acceptable. Sample "
-                         "will look for a suitable plugin for device "
-                         "specified (CPU by default)")
+                         "CPU, GPU, FPGA or MYRIAD is acceptable(CPU by default)")
     parser.add_argument("-t", "--threshold", type=float, default=0.5,
                     help="Probability threshold for detections filtering"
                          "(0.5 by default)")
     parser.add_argument("-e", "--extensions", required=False, type=str,
                         default=None,
-                        help="MKLDNN (CPU)-targeted custom layers."
-                             "Absolute path to a shared library with the"
-                             "kernels impl.")
+                        help="Extensions for custom layers support")
     parser.add_argument("-flag", "--visualization", type=str, required=False, nargs='+',
                         default=[],
                         help="Example: --flag [fd] [fl] [hp] [ge] (No flag is required,each represents stages of the pipeline)")
@@ -48,6 +44,7 @@ def build_argparser():
 def main():
     args = build_argparser().parse_args()
     input_file = args.input
+    logger = log.getLogger()
     if input_file == "CAM":
         input_feeder = InputFeeder("cam")
     else:
@@ -90,21 +87,21 @@ def main():
         else:
             continue
         if "fd" in args.visualization:
-            cv2.rectangle(frame, (face_coordinates[0], face_coordinates[1]), (face_coordinates[2], face_coordinates[3]), (36,255,12), 2)
-            cv2.putText(frame, "Face Detected", (face_coordinates[0], face_coordinates[1]-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
+            cv2.rectangle(frame, (face_coordinates[0], face_coordinates[1]), (face_coordinates[2], face_coordinates[3]), (36, 255, 12), 2)
+            cv2.putText(frame, "Face Detected", (face_coordinates[0], face_coordinates[1]-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36, 255, 12), 2)
 
         left_eye_img, righ_eye_img, eye_coords = face_landmark_detector.predict(face_image)
         if "fl" in args.visualization:
-            frame_eye_coords_min = np.array(eye_coords)[:,:2] + np.array(face_coordinates)[:2]
-            frame_eye_coords_max = np.array(eye_coords)[:,2:] + np.array(face_coordinates)[:2]
-            cv2.rectangle(frame, (frame_eye_coords_min[0][0],frame_eye_coords_min[0][1]), (frame_eye_coords_max[0][0],frame_eye_coords_max[0][1]), (36,255,12), 2)
-            cv2.rectangle(frame, (frame_eye_coords_min[1][0],frame_eye_coords_min[1][1]), (frame_eye_coords_max[1][0],frame_eye_coords_max[1][1]), (36,255,12), 2)
+            frame_eye_coords_min = np.array(eye_coords)[:, :2] + np.array(face_coordinates)[:2]
+            frame_eye_coords_max = np.array(eye_coords)[:, 2:] + np.array(face_coordinates)[:2]
+            cv2.rectangle(frame, (frame_eye_coords_min[0][0], frame_eye_coords_min[0][1]), (frame_eye_coords_max[0][0], frame_eye_coords_max[0][1]), (36, 255,12), 2)
+            cv2.rectangle(frame, (frame_eye_coords_min[1][0], frame_eye_coords_min[1][1]), (frame_eye_coords_max[1][0], frame_eye_coords_max[1][1]), (36, 255, 12), 2)
 
         head_pose_estimate = head_pose_estimator.predict(face_image)
         if "hp" in args.visualization:
             cv2.putText(frame,
                         "yaw:{:.1f}|pitch:{:.1f}|roll:{:.1f}".format(*head_pose_estimate),
-                        (20, 35), cv2.FONT_HERSHEY_COMPLEX, 1.2, (36,255,12), 3)
+                        (20, 35), cv2.FONT_HERSHEY_COMPLEX, 1.2, (36, 255, 12), 3)
 
         mouse_coordinate, gaze_vector = gaze_estimator.predict(left_eye_img, righ_eye_img, head_pose_estimate)
         if "ge" in args.visualization:
@@ -134,12 +131,13 @@ def main():
 
             cv2.arrowedLine(frame, origin, tip, (0, 0, 255), 3, tipLength=0.3)
 
-        cv2.imshow('frame',cv2.resize(frame,(width, height)))
+        cv2.imshow('frame', cv2.resize(frame, (width, height)))
         mouse_controller.move(mouse_coordinate[0], mouse_coordinate[1])
 
         if pressed_key == 27:
-            logger_object.error("exit key is pressed..")
+            logger.error("exit key is pressed..")
             break
+
 
 if __name__ == "__main__":
     main()
